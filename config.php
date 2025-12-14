a801c26a86f2dc3d5f027c394a124f964e857251
tien <hoangtien01652579562@gmail.com>
2025-12-14 20:38:22 +0700

    push lan 6

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-$host = "localhost";
-$dbname = "notes_db";
-$username = "root";
-$password = "";
+define("HOST", "sql205.infinityfree.com");          // MySQL Hostname
+define("DATABASE", "if0_40679954_notes_db");       // Database Name
+define("USERNAME", "if0_40679954");                // MySQL Username
+define("PASSWORD", "Hoangtien12345");              // MySQL Password
 
 try {
     $conn = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8",
-        $username,
-        $password
+        "mysql:host=" . HOST . ";dbname=" . DATABASE . ";charset=utf8",
+        USERNAME,
+        PASSWORD
     );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Lỗi kết nối database: " . $e->getMessage());
+    die("Kết nối database thất bại: " . $e->getMessage());
 }
+?>
